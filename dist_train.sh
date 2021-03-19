0cdb8da3c425dd061066ccd8a66a024f0623d97f
Uzair <uzair.789@hotmail.com>
2021-03-19 13:28:25 -0400

    [ironman] Training for binary pvt small

--- dist_train.sh
+++ dist_train.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 export NCCL_LL_THRESHOLD=0
+export CUDA_VISIBLE_DEVICES='0,1,2,3'
 
-ARCH=$1
-GPUS=$2
-OUT_PATH=$3
+ARCH='pvt_small' # $1
+GPUS=4 # $2
+OUT_PATH='./checkpoints' # $3
 PORT=${PORT:-29500}
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
