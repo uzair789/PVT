3dd5a7a993c8b736994762fc42195f48058e51d9
Uzair <uzair.789@hotmail.com>
2021-03-19 13:21:14 -0400

    [ironman] Made changes on the pvt code to implement the binarization from Hai's code. Running the classification first for binary pvt small retinanet 640

--- binary_pvt.py
+++ binary_pvt.py
@@ -6,10 +6,9 @@ from functools import partial
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from timm.models.registry import register_model
 from timm.models.vision_transformer import _cfg
-
-__all__ = [
-    'pvt_tiny', 'pvt_small', 'pvt_medium', 'pvt_large'
-]
+from mmdet.models.builder import BACKBONES
+from mmdet.utils import get_root_logger
+from mmcv.runner import load_checkpoint
 
 class BinaryActivation(nn.Module):
     def __init__(self):
@@ -88,21 +87,24 @@ class BinaryLinear(nn.Module):
         if self.bias:
             y = self.bias(y)
         return y
-
+        
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
-        self.fc1 = BinaryLinear(in_features, hidden_features) # nn.Linear(in_features, hidden_features)
-        self.act = BinaryActivation() # act_layer()
-        self.fc2 = BinaryLinear(in_features, out_features) # nn.Linear(hidden_features, out_features)
+        self.binary_act = BinaryActivation()
+        self.fc1 = BinaryLinear(in_features, hidden_features) #nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = BinaryLinear(hidden_features, out_features) #nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
     def forward(self, x):
+        x = self.binary_act(x)
         x = self.fc1(x)
         x = self.act(x)
         x = self.drop(x)
+        x = self.binary_act(x)
         x = self.fc2(x)
         x = self.drop(x)
         return x
@@ -118,11 +120,13 @@ class Attention(nn.Module):
         head_dim = dim // num_heads
         self.scale = qk_scale or head_dim ** -0.5
 
-        self.q = BinaryLinear(dim, dim, bias=qkv_bias) # nn.Linear(dim, dim, bias=qkv_bias)
-        self.kv = BinaryLinear(dim, dim * 2, bias=qkv_bias) # nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.q = BinaryLinear(dim, dim, bias=qkv_bias) #nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = BinaryLinear(dim, dim * 2, bias=qkv_bias) #nn.Linear(dim, dim * 2, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = BinaryLinear(dim, dim) # nn.Linear(dim, dim)
+        self.proj = BinaryLinear(dim, dim) #nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
+
+        self.binary_act = BinaryActivation()
 
         self.sr_ratio = sr_ratio
         if sr_ratio > 1:
@@ -132,12 +136,15 @@ class Attention(nn.Module):
 
     def forward(self, x, H, W):
         B, N, C = x.shape
+        x = self.binary_act(x)
         q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
         if self.sr_ratio > 1:
             x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.binary_act(x_)
             x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
             x_ = self.norm(x_)
+            x_ = self.binary_act(x_)
             kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         else:
             kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
@@ -181,7 +188,7 @@ class PatchEmbed(nn.Module):
     """ Image to Patch Embedding
     """
 
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768, isBin=True):
         super().__init__()
         img_size = to_2tuple(img_size)
         patch_size = to_2tuple(patch_size)
@@ -192,12 +199,18 @@ class PatchEmbed(nn.Module):
             f"img_size {img_size} should be divided by patch_size {patch_size}."
         self.H, self.W = img_size[0] // patch_size[0], img_size[1] // patch_size[1]
         self.num_patches = self.H * self.W
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
         self.norm = nn.LayerNorm(embed_dim)
+        self.isBin = isBin
+        if self.isBin:
+            self.binary_act = BinaryActivation()
+            self.proj = HardBinaryConv(in_chans, embed_dim, kernel_size=patch_size[0], stride=patch_size)
+        else:
+            self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
         B, C, H, W = x.shape
-
+        if self.isBin:
+            x = self.binary_act(x)
         x = self.proj(x).flatten(2).transpose(1, 2)
         x = self.norm(x)
         H, W = H // self.patch_size[0], W // self.patch_size[1]
@@ -205,18 +218,19 @@ class PatchEmbed(nn.Module):
         return x, (H, W)
 
 
-class PyramidVisionTransformer(nn.Module):
+class PyramidVisionBinTransformer(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dims=[64, 128, 256, 512],
                  num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
                  attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
-                 depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1]):
+                 depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], F4=False):
         super().__init__()
         self.num_classes = num_classes
         self.depths = depths
+        self.F4 = F4
 
         # patch_embed
         self.patch_embed1 = PatchEmbed(img_size=img_size, patch_size=patch_size, in_chans=in_chans,
-                                       embed_dim=embed_dims[0])
+                                       embed_dim=embed_dims[0], isBin=False)
         self.patch_embed2 = PatchEmbed(img_size=img_size // 4, patch_size=2, in_chans=embed_dims[0],
                                        embed_dim=embed_dims[1])
         self.patch_embed3 = PatchEmbed(img_size=img_size // 8, patch_size=2, in_chans=embed_dims[1],
@@ -263,43 +277,36 @@ class PyramidVisionTransformer(nn.Module):
             drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[cur + i], norm_layer=norm_layer,
             sr_ratio=sr_ratios[3])
             for i in range(depths[3])])
-        self.norm = norm_layer(embed_dims[3])
-
-        # cls_token
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dims[3]))
-
-        # classification head
-        self.head = nn.Linear(embed_dims[3], num_classes) if num_classes > 0 else nn.Identity()
 
         # init weights
         trunc_normal_(self.pos_embed1, std=.02)
         trunc_normal_(self.pos_embed2, std=.02)
         trunc_normal_(self.pos_embed3, std=.02)
         trunc_normal_(self.pos_embed4, std=.02)
-        trunc_normal_(self.cls_token, std=.02)
         self.apply(self._init_weights)
+
+    def init_weights(self, pretrained=None):
+        if isinstance(pretrained, str):
+            logger = get_root_logger()
+            load_checkpoint(self, pretrained, map_location='cpu', strict=False, logger=logger)
 
     def reset_drop_path(self, drop_path_rate):
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(self.depths))]
         cur = 0
         for i in range(self.depths[0]):
             self.block1[i].drop_path.drop_prob = dpr[cur + i]
-            # print(dpr[cur + i])
 
         cur += self.depths[0]
         for i in range(self.depths[1]):
             self.block2[i].drop_path.drop_prob = dpr[cur + i]
-            # print(dpr[cur + i])
 
         cur += self.depths[1]
         for i in range(self.depths[2]):
             self.block3[i].drop_path.drop_prob = dpr[cur + i]
-            # print(dpr[cur + i])
 
         cur += self.depths[2]
         for i in range(self.depths[3]):
             self.block4[i].drop_path.drop_prob = dpr[cur + i]
-            # print(dpr[cur + i])
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -310,69 +317,66 @@ class PyramidVisionTransformer(nn.Module):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        # return {'pos_embed', 'cls_token'} # has pos_embed may be better
-        return {'cls_token'}
-
-    def get_classifier(self):
-        return self.head
-
-    def reset_classifier(self, num_classes, global_pool=''):
-        self.num_classes = num_classes
-        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-    # def _get_pos_embed(self, pos_embed, patch_embed, H, W):
-    #     if H * W == self.patch_embed1.num_patches:
-    #         return pos_embed
-    #     else:
-    #         return F.interpolate(
-    #             pos_embed.reshape(1, patch_embed.H, patch_embed.W, -1).permute(0, 3, 1, 2),
-    #             size=(H, W), mode="bilinear").reshape(1, -1, H * W).permute(0, 2, 1)
+    def _get_pos_embed(self, pos_embed, patch_embed, H, W):
+        if H * W == self.patch_embed1.num_patches:
+            return pos_embed
+        else:
+            return F.interpolate(
+                pos_embed.reshape(1, patch_embed.H, patch_embed.W, -1).permute(0, 3, 1, 2),
+                size=(H, W), mode="bilinear").reshape(1, -1, H * W).permute(0, 2, 1)
 
     def forward_features(self, x):
+        outs = []
+
         B = x.shape[0]
 
         # stage 1
         x, (H, W) = self.patch_embed1(x)
-        x = x + self.pos_embed1
+        pos_embed1 = self._get_pos_embed(self.pos_embed1, self.patch_embed1, H, W)
+        x = x + pos_embed1
         x = self.pos_drop1(x)
         for blk in self.block1:
             x = blk(x, H, W)
         x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
 
         # stage 2
         x, (H, W) = self.patch_embed2(x)
-        x = x + self.pos_embed2
+        pos_embed2 = self._get_pos_embed(self.pos_embed2, self.patch_embed2, H, W)
+        x = x + pos_embed2
         x = self.pos_drop2(x)
         for blk in self.block2:
             x = blk(x, H, W)
         x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
 
         # stage 3
         x, (H, W) = self.patch_embed3(x)
-        x = x + self.pos_embed3
+        pos_embed3 = self._get_pos_embed(self.pos_embed3, self.patch_embed3, H, W)
+        x = x + pos_embed3
         x = self.pos_drop3(x)
         for blk in self.block3:
             x = blk(x, H, W)
         x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
 
         # stage 4
         x, (H, W) = self.patch_embed4(x)
-        cls_tokens = self.cls_token.expand(B, -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed4
+        pos_embed4 = self._get_pos_embed(self.pos_embed4[:, 1:], self.patch_embed4, H, W)
+        x = x + pos_embed4
         x = self.pos_drop4(x)
         for blk in self.block4:
             x = blk(x, H, W)
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        outs.append(x)
 
-        x = self.norm(x)
-
-        return x[:, 0]
+        return outs
 
     def forward(self, x):
         x = self.forward_features(x)
-        x = self.head(x)
+
+        if self.F4:
+            x = x[3:4]
 
         return x
 
@@ -388,71 +392,28 @@ def _conv_filter(state_dict, patch_size=16):
     return out_dict
 
 
-@register_model
-def pvt_tiny(pretrained=False, **kwargs):
-    model = PyramidVisionTransformer(
-        patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
-        # drop_rate=0.0, drop_path_rate=0.1)
-        **kwargs)
-    model.default_cfg = _cfg()
-    # if pretrained:
-    #     checkpoint = torch.hub.load_state_dict_from_url(
-    #         url=None,
-    #         map_location="cpu", check_hash=True
-    #     )
-    #     model.load_state_dict(checkpoint["model"])
-
-    return model
+@BACKBONES.register_module()
+class pvt_bin_tiny(PyramidVisionBinTransformer):
+    def __init__(self, **kwargs):
+        super(pvt_bin_tiny, self).__init__(
+            patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4],
+            qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[2, 2, 2, 2],
+            sr_ratios=[8, 4, 2, 1], drop_rate=0.0, drop_path_rate=0.1)
 
 
-@register_model
-def pvt_small(pretrained=False, **kwargs):
-    model = PyramidVisionTransformer(
-        patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], **kwargs)
-    model.default_cfg = _cfg()
-    # if pretrained:
-    #     checkpoint = torch.hub.load_state_dict_from_url(
-    #         url=None,
-    #         map_location="cpu", check_hash=True
-    #     )
-    #     model.load_state_dict(checkpoint["model"])
-
-    return model
+@BACKBONES.register_module()
+class pvt_bin_small(PyramidVisionBinTransformer):
+    def __init__(self, **kwargs):
+        super(pvt_bin_small, self).__init__(
+            patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4],
+            qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3],
+            sr_ratios=[8, 4, 2, 1], drop_rate=0.0, drop_path_rate=0.1)
 
 
-@register_model
-def pvt_medium(pretrained=False, **kwargs):
-    model = PyramidVisionTransformer(
-        patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 18, 3], sr_ratios=[8, 4, 2, 1],
-        # drop_rate=0.0, drop_path_rate=0.05)
-        **kwargs)
-    model.default_cfg = _cfg()
-    # if pretrained:
-    #     checkpoint = torch.hub.load_state_dict_from_url(
-    #         url=None,
-    #         map_location="cpu", check_hash=True
-    #     )
-    #     model.load_state_dict(checkpoint["model"])
-
-    return model
-
-
-@register_model
-def pvt_large(pretrained=False, **kwargs):
-    model = PyramidVisionTransformer(
-        patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 8, 27, 3], sr_ratios=[8, 4, 2, 1],
-        # drop_rate=0.0, drop_path_rate=0.02)
-        **kwargs)
-    model.default_cfg = _cfg()
-    # if pretrained:
-    #     checkpoint = torch.hub.load_state_dict_from_url(
-    #         url=None,
-    #         map_location="cpu", check_hash=True
-    #     )
-    #     model.load_state_dict(checkpoint["model"])
-
-    return model
+@BACKBONES.register_module()
+class pvt_bin_small_f4(PyramidVisionBinTransformer):
+    def __init__(self, **kwargs):
+        super(pvt_bin_small_f4, self).__init__(
+            patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4],
+            qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3],
+            sr_ratios=[8, 4, 2, 1], drop_rate=0.0, drop_path_rate=0.1, F4=True)
